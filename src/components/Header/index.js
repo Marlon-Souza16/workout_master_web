e862d72347c86589346acc30fe620f786318809f
Marlon de Souza <marlondesouzajlle@hotmail.com>
2024-03-18 19:36:41 -0300

    feat: initial commit

---
+++ src/components/Header/index.js
@@ -0,0 +1,17 @@
+import HeaderLink from '@/components/HeaderLink';
+
+export default function Header ()
+{
+  return (
+    <header className='Header'>
+      <nav>
+        <HeaderLink url="/Home">Home</HeaderLink>
+        <HeaderLink url="/Exercicio">Favoritos</HeaderLink>
+        <HeaderLink url="/Alongamentos">Favoritos</HeaderLink>
+        <HeaderLink url="/Rotina">Favoritos</HeaderLink>
+        <HeaderLink url="/Ajuda">Favoritos</HeaderLink>
+        <HeaderLink url="/Login">Favoritos</HeaderLink>
+      </nav>
+    </header>
+  );
+}
---
+++ src/pages/_app.js
@@ -0,0 +1,7 @@
+import "@/styles/globals.css";
+import "@/components/HeaderLink/HeaderLink.css"
+import "@/components/Header/Header.css"
+
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
